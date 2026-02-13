0a0155a38b253118286c09961da5429de7ceb8be
koyaharikrishna <koyaharikrishna@koyas-MacBook-Air.local>
2026-02-13 16:49:54 +0530

    second python code

--- add.py
+++ add.py
@@ -1,9 +1,17 @@
+import sys
+
 def add_numbers(a, b):
     return a + b
 
 if __name__ == "__main__":
-    num1 = int(input("Enter first number: "))
-    num2 = int(input("Enter second number: "))
+
+    if len(sys.argv) == 3:
+        num1 = int(sys.argv[1])
+        num2 = int(sys.argv[2])
+    else:
+        print("No command-line arguments detected.")
+        num1 = int(input("Enter first number: "))
+        num2 = int(input("Enter second number: "))
 
     result = add_numbers(num1, num2)
 
